a93101b6124f4de3aa282986d998092b05789376
Yuzumama <ilin.taiwan1920@gmail.com>
2024-04-27 10:46:34 +0900

    first commit

---
+++ js/left_bar_functions.js
@@ -0,0 +1,32 @@
+// 登出帳號, 回到登入畫面的index.php
+$("#btn_logout").on("click", function () {
+    if (confirm("Are you sure to log-out?")) {
+        window.location.href = "logout.php";
+    }
+});
+
+$("#btn_ai_recommend").on("click", function() {
+    getDevicePage("try_recommendation.php", true);
+});
+
+$("#btn_bookshelf").on("click", function(){
+    getDevicePage("bookshelf.php", true);
+});
+
+$("#btn_service").on("click", function() {
+    $("#left_bar_main").css({
+        left: "-230px",
+    });
+    $("#left_bar_service").css({
+        left: "20px",
+    });
+});
+
+function backtoMain(originalBarId) {
+    $("#" + originalBarId).css({
+        left: "270px",
+    });
+    $("#left_bar_main").css({
+        left: "20px",
+    });
+}
